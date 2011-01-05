5fa3abbb562cbe2174e9681e37e39c54816389a6
Mihai Tarnovan <mihai.tarnovan@cubus.ro>
2011-01-05 14:36:48 +0200

    * removed some unnecessary code

--- geocoder.rb
+++ geocoder.rb
@@ -1,14 +1,6 @@
 class GeoCoder
   class UnknownIp < StandardError; end
   
-  COUNTRY_CODE_TO_LANG = {
-    :at => :de,
-    :de => :de,
-    :ch => :de,
-    :ro => :ro,
-    :md => :ro
-  }
-  
   @@retry_count = 0
   
   def self.country_code_from_ip(ip)
@@ -33,10 +25,4 @@ class GeoCoder
     end
   end
   
-  def self.lang_from_ip(ip, fallback_lang = :en)
-    COUNTRY_CODE_TO_LANG[country_code_from_ip(ip).downcase.to_sym] || fallback_lang
-  rescue UnknownIp
-    #Rails.logger.warn "Could not geolocate IP: #{ip}"
-    fallback_lang
-  end
 end
